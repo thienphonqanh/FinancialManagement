32b2d95de8affde2f92c31f1f09902bf6b9df8ae
thienphonqanh <thienphonqanh@gmail.com>
2024-04-17 18:25:07 +0700

    Add DatabaseService For User

---
+++ src/services/database.services.ts
@@ -0,0 +1,35 @@
+import { MongoClient, Db, Collection } from 'mongodb'
+import { config } from 'dotenv'
+import User from '~/models/schemas/User.schemas'
+
+config()
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@twitter.kuuplfy.mongodb.net/?retryWrites=true&w=majority&appName=Twitter`
+
+class DatabaseService {
+  private client: MongoClient
+  private db: Db
+
+  constructor() {
+    this.client = new MongoClient(uri)
+    this.db = this.client.db(process.env.DB_NAME)
+  }
+
+  async connect() {
+    try {
+      // Send a ping to confirm a successful connection
+      await this.db.command({ ping: 1 })
+      console.log('Pinged your deployment. You successfully connected to MongoDB!')
+    } catch (error) {
+      console.log('Error', error)
+      throw error
+    }
+  }
+
+  get users(): Collection<User> {
+    return this.db.collection(process.env.DB_USERS_COLLECTION as string)
+  }
+}
+
+const databaseService = new DatabaseService()
+export default databaseService
